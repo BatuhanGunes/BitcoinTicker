e5778543ce79839adf9dac90c971336f1ad89e60
Batuhan Güneş <batuhangunes001@gmail.com>
2021-04-25 10:25:01 +0300

    API created and gecko api queries added

--- app/src/main/java/loodos/droid/bitcointicker/TickerApplication.kt
+++ app/src/main/java/loodos/droid/bitcointicker/TickerApplication.kt
@@ -1,6 +1,7 @@
 package loodos.droid.bitcointicker
 
 import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
 import loodos.droid.bitcointicker.data.firebase.FirebaseSource
 import loodos.droid.bitcointicker.data.repositories.UserRepository
 import loodos.droid.bitcointicker.ui.auth.AuthViewModelFactory
@@ -12,16 +13,28 @@ import org.kodein.di.generic.bind
 import org.kodein.di.generic.instance
 import org.kodein.di.generic.provider
 import org.kodein.di.generic.singleton
+import timber.log.Timber
 
-class FirebaseApplication : Application(), KodeinAware {
+@HiltAndroidApp
+class TickerApplication : Application(), KodeinAware {
+
+    override fun onCreate() {
+        super.onCreate()
+        initTimber()
+    }
 
     override val kodein = Kodein.lazy {
-        import(androidXModule(this@FirebaseApplication))
+        import(androidXModule(this@TickerApplication))
 
         bind() from singleton { FirebaseSource() }
         bind() from singleton { UserRepository(instance()) }
         bind() from provider { AuthViewModelFactory(instance()) }
         bind() from provider { HomeViewModelFactory(instance()) }
+    }
 
+    private fun initTimber() {
+        if (BuildConfig.DEBUG) {
+            Timber.plant(Timber.DebugTree())
+        }
     }
 }
---
+++ app/src/main/java/loodos/droid/bitcointicker/api/models/GenericResponse.kt
@@ -0,0 +1,6 @@
+package loodos.droid.bitcointicker.api.models
+
+data class GenericResponse(
+    val code: Int,
+    val message: String
+)
