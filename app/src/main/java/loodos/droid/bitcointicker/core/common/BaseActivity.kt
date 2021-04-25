0f39d80829a828611fd76c80b6388e592b270b92
Batuhan Güneş <batuhangunes001@gmail.com>
2021-04-25 10:26:52 +0300

    Core structures of the application have been added

---
+++ app/src/main/java/loodos/droid/bitcointicker/core/common/BaseActivity.kt
@@ -0,0 +1,18 @@
+package loodos.droid.bitcointicker.core.common
+
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+
+/**
+ * [BaseActivity] contains common methods and properties and all activities must extend this class
+ */
+abstract class BaseActivity : AppCompatActivity() {
+
+    fun showToast(message: String, duration: Int = Toast.LENGTH_SHORT) {
+        Toast.makeText(
+            this,
+            message,
+            duration
+        ).show()
+    }
+}
